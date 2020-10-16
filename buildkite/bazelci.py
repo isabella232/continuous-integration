527580d7b926953ab7511f63f94c2d42818a5d30
mai93 <70515749+mai93@users.noreply.github.com>
2020-10-16 14:25:27 +0200

    Testing viewing Gerrit review link

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -2166,8 +2166,7 @@ def print_project_pipeline(
 
     pipeline_steps = []
     # If the repository is hosted on Git-on-borg, we show the link to the commit Gerrit review
-    if is_git_on_borg_repo(git_repository):
-        show_gerrit_review_link(git_repository, pipeline_steps)
+    show_gerrit_review_link(git_repository, pipeline_steps)
 
     task_configs = filter_tasks_that_should_be_skipped(task_configs, pipeline_steps)
 
